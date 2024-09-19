8f821a3540c8ff08f2947d1db5d8e8326d4aa5ce
danivelop <mofg266@gmail.com>
2024-09-20 04:02:50 +0900

    fix: 댓글UI 다크모드 대응

--- src/features/comment/ui/Utterances.tsx
+++ src/features/comment/ui/Utterances.tsx
@@ -2,26 +2,57 @@
 
 import { useEffect, useRef } from 'react';
 
-function Utterances() {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const isInitialized = useRef(false);
+import { useStore } from '@/store';
 
-  useEffect(() => {
-    if (!containerRef.current || isInitialized.current) return;
+function Utterances() {
+  const isDarkMode = useStore((state) => state.themeSlice.isDarkMode);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const shouldReinitialize = useRef(false);
+  const isScriptLoading = useRef(false);
+  const isDarkModeRef = useRef(isDarkMode);
+  isDarkModeRef.current = isDarkMode;
+
+  const createScriptRef = useRef(function createScript() {
+    if (!containerRef.current) {
+      return;
+    }
+    if (containerRef.current.firstChild) {
+      containerRef.current.removeChild(containerRef.current.firstChild as Node);
+    }
 
     const script = document.createElement('script');
     script.setAttribute('src', 'https://utteranc.es/client.js');
     script.setAttribute('repo', 'danivelop/leaving-mark');
     script.setAttribute('issue-term', 'pathname');
     script.setAttribute('label', 'comment');
-    script.setAttribute('theme', 'github-light');
+    script.setAttribute(
+      'theme',
+      isDarkModeRef.current ? 'github-dark' : 'github-light',
+    );
     script.setAttribute('crossorigin', 'anonymous');
     script.async = true;
 
     containerRef.current.appendChild(script);
+    isScriptLoading.current = true;
 
-    isInitialized.current = true;
-  }, []);
+    script.onload = () => {
+      isScriptLoading.current = false;
+
+      if (shouldReinitialize.current) {
+        shouldReinitialize.current = false;
+        createScript();
+      }
+    };
+  });
+
+  useEffect(() => {
+    if (isScriptLoading.current) {
+      shouldReinitialize.current = true;
+    } else {
+      createScriptRef.current();
+    }
+  }, [isDarkMode]);
 
   return <div ref={containerRef} />;
 }
